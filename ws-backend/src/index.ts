ec0152be357c4642a29a657ffb84a1cc9521c490
harsh-05 <harshverma09022001@gmail.com>
2026-05-16 17:13:44 +0530

    learned property addition on ws

--- ws-backend/src/index.ts
+++ ws-backend/src/index.ts
@@ -1,22 +1,30 @@
-import WebSocket, {WebSocketServer} from 'ws';
+import WebSocket, {WebSocketServer, type ClientOptions} from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+interface ExtwebSocket extends WebSocket {
+    userNumber: number;
+}
 
+let userNUmber = 1;
+wss.on('connection', function connection(ws:ExtwebSocket, ) {
+ 
+    ws.userNumber = userNUmber++;    
 
-wss.on('connection', function connection(ws) {
-    ws.on("open", (w:any) => (console.log(w)));
+    ws.on('error', (e) => (console.log(e)));
 
     ws.on('message', (mes) => {
         wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN && client !== ws) {
-                client.send(mes);
+               
+                client.send(`User ${ws.userNumber} sends message : ` + mes.toString());
             }
         })
-        console.log("received" + mes.toString);
+        console.log("received " + mes.toString());
+        console.log("received " + mes.toLocaleString());
+        console.log("received " + mes);
     });
 
-    ws.close()
 })
 
-wss.close()
+

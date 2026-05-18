de49bfc99492e8fdee0455d60aaa64c4a7b65cf3
harsh-05 <harshverma09022001@gmail.com>
2026-05-18 17:32:06 +0530

    added the react client, for a live button counter

---
+++ ws-client/src/App.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef, useState } from "react"
+
+
+function App() {
+  const [number, setNumber] = useState(0);
+  const wsRef = useRef<WebSocket>(null);
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    wsRef.current = ws;
+
+    ws.onopen = () => {
+      console.log("client is connected !");
+    }
+    
+    ws.onmessage = (e) => {
+        const data = JSON.parse(e.data)
+      setNumber(data.number);
+    }
+
+    return () => {
+      ws.close();
+
+    }
+
+  }, [])
+
+  function senddata() {
+    const data = JSON.stringify({ type: "increment" });
+    wsRef.current?.send(data);
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button onClick={()=>{ senddata()}} className="bg-blue-300 py-2 px-4 rounded-md active:bg-blue-400 active:text-white shadow-xl active:shadow-lg shadow-blue-400">{`Clicked : ${number} times` }</button>
+    </div>
+  )
+      
+}
+
+export default App
